5b40c3eb3e2dfc18442af3867e31b96e1ab8c6aa
Hizuro <hizuro@gmx.net>
2016-05-22 22:06:35 +0200

    update localization example file

--- localizations/example.lua
+++ localizations/example.lua
@@ -3,16 +3,39 @@ local addon,ns = ...
 local L = ns.L;
 
 if (LOCALE_<code>) then
+	L["Adding applicant %s to your friendlist."] = "";
 	L["Addon inactive."] = "";
 	L["Addon loaded..."] = "";
-
-	L["Applicant %s added to your friendlist."] = "";
-
+	L["Click to open option menu"] = "";
+	L["Close %s"] = "";
+	L["Commandline options for %s"] = "";
+	L["Commands:"] = "";
+	L["Currently no applicants found"] = "";
+	L["Do you really want to decline %s's guild application?"] = "";
+	L["If the frame out of screen, you can reset its position with this option"] = "";
+	L["Left-click || to toggle tracker frame"] = "";
+	L["Minimap"] = "";
+	L["No comment..."] = "";
+	L["Offline applicants"] = "";
+	L["Popup frame if applicant online"] = "";
+	L["Reset addon settings"] = "";
+	L["Reset frame position"] = "";
+	L["Right-click || to open option menu"] = "";
+	L["Show/Hide GuildApplicantTracker"] = "";
+	L["Show/Hide minimap icon"] = "";
+	L["Show/Hide offline applicants"] = "";
+	L["Show/Hide tracker frame"] = "";
+	L["Show minimap button"] = "";
+	L["Show offline applicants"] = "";
+	L["Show or hide minimap button"] = "";
+	L["Show or hide offline applicants"] = "";
+	L["Usage: /gat <command>"] = "";
+	L["Warning:"] = "";
 	L["You are not in a guild."] = "";
 	L["You can not invite players."] = "";
-
 	L["You want to invite %s into this guild?"] = "";
-	L["Do you really want to decline %s's guild application?"] = "";
+	L["has decline your invitation..."] = "";
+	L["has decline your invitation automaticly (by interface option)..."] = "";
 end
 
 --[[
