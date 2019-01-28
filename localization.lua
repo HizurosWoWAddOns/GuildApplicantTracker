c8d4a11a744e6f8f177d914da3bb4a3b55661aa5
Hizuro <hizuro@gmx.net>
2019-01-28 07:26:31 +0100

    move localization into a single file

---
+++ localization.lua
@@ -0,0 +1,54 @@
+
+local addon, ns = ...
+
+local L = {};
+ns.L = setmetatable(L,{__index=function(t,k)
+	local v = tostring(k)
+	rawset(t,k,v)
+	return v
+end});
+
+-- Do you want to help localize this addon?
+-- https://wow.curseforge.com/projects/guildapplicanttracker/localization
+
+--@localization(locale="enUS", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+
+if LOCALE_deDE then
+--@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_esES then
+--@localization(locale="esES", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_esMX then
+--@localization(locale="esMX", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_frFR then
+--@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_itIT then
+--@localization(locale="itIT", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_koKR then
+--@localization(locale="koKR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_ptBR or LOCALE_ptPT then
+--@localization(locale="ptBR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_ruRU then
+--@localization(locale="ruRU", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_zhCN then
+--@localization(locale="zhCN", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_zhTW then
+--@localization(locale="zhTW", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
--- localizations/core.lua
+++
@@ -1,9 +0,0 @@
-
-local addon, ns = ...
-
-ns.L = setmetatable({},{__index=function(t,k)
-	local v = tostring(k)
-	rawset(t,k,v)
-	return v
-end})
-
